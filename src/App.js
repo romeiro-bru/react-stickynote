b3a76b66258d654cecf0db74db5e6aae79d40cf0
Bruna Romeiro <56081906+romeiro-bru@users.noreply.github.com>
2021-05-03 16:05:42 -0300

    Add rmv item function

--- src/App.js
+++ src/App.js
@@ -2,38 +2,35 @@ import "./styles.css";
 import { Header } from "./Header/Header";
 import { useState } from "react";
 import add from "./add.svg";
-import { AiFillCheckCircle } from "react-icons/ai";
+import { CgCloseR } from "react-icons/cg";
 
 export default function App() {
   const [input, setInput] = useState("");
   const [list, setList] = useState([]);
 
-  // console.log(`input => ${input}`);
+  console.log(`input => ${input}`);
 
   const handleInput = (event) => {
     setInput(event.target.value);
-    console.log(event.target.value);
+    // console.log(event.target.value);
   };
-
   const handleSubmit = (event) => {
-    if (input) {
-      setList([...list, input]);
-    }
+    input && setList([...list, input]);
     event.preventDefault();
   };
 
-  const handleRemove = () => {
-    console.log("removido");
+  const handleRemove = (itemIndex) => {
+    setList(list.filter((_, index) => index !== itemIndex));
   };
 
-  console.log(`note => ${list}`);
+  console.log(`list => ${list}`);
 
   return (
     <div className="App">
       <Header />
       <form>
         <input onChange={handleInput} />
-        <button type="submit" onClick={handleSubmit}>
+        <button onClick={handleSubmit}>
           <img src={add} alt="add" />
         </button>
       </form>
@@ -42,8 +39,8 @@ export default function App() {
           {list.map((item, index) => (
             <li className="notes" key={index}>
               {item}
-              <button onClick={handleRemove}>
-                <AiFillCheckCircle size={25} className="check" />
+              <button onClick={() => handleRemove(index)}>
+                <CgCloseR className="check" />
               </button>
             </li>
           ))}
