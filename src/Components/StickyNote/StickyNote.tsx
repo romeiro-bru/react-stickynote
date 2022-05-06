3b5531344a312fe93ba607f81ca15ef6e13c38c8
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-06 18:34:24 -0300

    REmove css file

--- src/Components/StickyNote/StickyNote.tsx
+++ src/Components/StickyNote/StickyNote.tsx
@@ -1,5 +1,4 @@
 import React, { SetStateAction } from "react";
-import "./style.css";
 import { CgCloseR } from "react-icons/cg";
 
 type StickyNote = {
@@ -16,7 +15,7 @@ export function StickyNote({ list, setList }: StickyNote) {
   return (
     <ul>
       {list.map((item, index) => (
-        <li className="shadow-lg py-4 px-2 my-6 mx-.5 justify-between flex text-left font-semibold rounded-sm bg-amber-200 notes" key={index}>
+        <li className="max-w-sm  even:bg-[#808f9e] shadow-lg py-4 px-2 mx-auto my-4 justify-between flex text-left font-semibold rounded-sm bg-amber-200" key={index}>
           {item}
           <button onClick={() => handleRemove(index)}>
             <CgCloseR className="text-lg mr-2 duration-300 hover:opacity-60" />
