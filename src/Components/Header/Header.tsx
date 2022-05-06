285455f122f48b38f907f460bb8e75abb524d720
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-06 18:16:08 -0300

    Add animation spin and remove css file

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./style.css";
 import atom from "../../assets/svg/atom.svg";
 
 export function Header() {
@@ -7,7 +6,7 @@ export function Header() {
     <>
       <h1 className="py-5 text-3xl text-fuchsia-400 font-bold">
         30 days of React
-        <img src={atom} alt="react" className="spin w-10 m-auto" />
+        <img src={atom} alt="react" className="animate-[spin_6s_infinite_linear] w-10 m-auto" />
       </h1>
       <h2 className="text-3xl text-fuchsia-400 font-bold">Day 6</h2>
     </>
