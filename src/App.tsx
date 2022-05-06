8aed6edbf01322b8952cd678a0e0074ae956907f
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-06 12:06:10 -0300

    Implement tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import "./styles.css";
 import { Header } from "./Components/Header/Header";
 import { Form } from "./Components/Form/Form";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="text-center min-h-screen bg-cyan-900">
       <Header />
       <Form />
     </div>
